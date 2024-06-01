5d78b2edd9852db8f5ec5cebe885063a8afab24e
lcw3176 <lcw3176@naver.com>
2024-06-01 13:16:10 +0900

    first commit

---
+++ src/main/java/com/comet/devjobplz/DevjobplzApplication.java
@@ -0,0 +1,13 @@
+package com.comet.devjobplz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevjobplzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevjobplzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/comet/devjobplz/DevjobplzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comet.devjobplz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevjobplzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
